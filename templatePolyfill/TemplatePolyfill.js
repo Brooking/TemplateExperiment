21df4756d880a5995f906b879174f7315c394af3
arthurb <arthurb@microsoft.com>
2018-09-19 12:12:48 -0700

    moved to ECMAScript2015 style classes

--- templatePolyfill/TemplatePolyfill.js
+++ templatePolyfill/TemplatePolyfill.js
@@ -1,294 +1,290 @@
-// This will become HTMLTemplateElement.instantiate()
-function HTMLTemplateElement_instantiate(thisTemplateElement, partProcessor, params) {
-    // instantiate the template
-    let documentFragment = document.importNode(thisTemplateElement.content,
-        true/*deep*/);
-    let templateInstance = new TemplateInstance(documentFragment);
+let ZERO_WIDTH_SPACE='&#x200B';
 
-    // stamp the template instance
-    templateInstance.update(partProcessor, params);
-    return templateInstance;
+// This will become HTMLTemplateElement.instantiate()
+function HTMLTemplateElement_instantiate(thisTemplateElement, partProcessor,
+                                         params) {
+  // instantiate the template
+  let documentFragment = document.importNode(thisTemplateElement.content,
+    true/*deep*/);
+  let templateInstance = new TemplateInstance(documentFragment);
+
+  // stamp the template instance
+  templateInstance.update(partProcessor, params);
+  return templateInstance;
 }
 
 //
 // TemplateInstance class
 //
-function TemplateInstance(documentFragment) {
-    this.originalDocumentFragment = documentFragment;
-    this.partArray = null;
-    this.documentFragment = null;
-}
+class TemplateInstance {
+  constructor(documentFragment) {
+    this._documentFragment = documentFragment;
 
-TemplateInstance.prototype.documentFragment = function () {
-    return this.documentFragment;
-}
-
-TemplateInstance.prototype.update = function (partProcessor, params) {
-    this.documentFragment = this.originalDocumentFragment;
     let parser = new PartParser(this);
-    this.partArray = parser.partArray;
+    this._partArray = parser.parts;
 
-    // process the parts in this template
-    if (partProcessor == null) {
-        partProcessor = this._defaultPartProcessor;
-    }
-    partProcessor(this.partArray, params);
-
-    // process any imbedded templates
-    let imbeddedTemplateArray = parser.imbeddedTemplateArray;
-    imbeddedTemplateArray.forEach(template => {
-        if (template.getAttribute('processor') == 'for-each') {
-            let parent = template.parentElement;
-            parent.removeChild(template);
-            let items = params["items"];
-            items.forEach(item => {
-                let templateInstance = HTMLTemplateElement_instantiate(template, partProcessor, item);
-                parent.appendChild(templateInstance.documentFragment);
-            });
-        } else {
-            window.alert("The only type of embedded template we know is 'for-each'");
-        }
+    // remove any internal templates, but keep track of where they go
+    this._foreachTemplates = [];
+    parser.internalTemplateNodes.forEach(templateNode => {
+      if (templateNode.getAttribute('processor') == 'for-each') {
+        let parent = templateNode.parentElement;
+        let templateRange = new Range();
+        templateRange.setStartBefore(templateNode);
+        templateRange.setEndAfter(templateNode);
+        templateRange.deleteContents();
+        let placeholder = new Text(ZERO_WIDTH_SPACE);
+        templateRange.insertNode(placeholder);
+        this._foreachTemplates.push({node: templateNode, range: templateRange,
+                                    placeholder: placeholder});
+      } else {
+          window.alert("The only type of embedded template we know is 'for-each'");
+      }
     })
-}
+  }
 
-TemplateInstance.prototype._defaultPartProcessor = function (parts, params) {
+  get documentFragment() {
+    return this._documentFragment;
+  }
+
+  update(partProcessor, params) {
+    // process the parts in this template
+    if (this._partArray !== undefined) {
+      if (partProcessor == null) {
+        partProcessor = TemplateInstance._defaultPartProcessor;
+      }
+      partProcessor(this._partArray, params);
+    }
+
+    // insert new template instances (rows)
+    this._foreachTemplates.forEach(templateStruct => {
+      let templateNode = templateStruct.node;
+      let range = templateStruct.range;
+      let itemsKey = templateNode.getAttribute('items');
+      itemsKey = itemsKey.substring(2, itemsKey.length - 2);
+      let items = params[itemsKey];
+
+      // walk them backwards because there is no range.append
+      for (let i = items.length; i > 0; i--) {
+        let item = items[i-1];
+        let documentFragment = document.importNode(templateNode.content,
+                                                   true/*deep*/);
+        let newTemplateInstance = new TemplateInstance(documentFragment);
+        newTemplateInstance.update(partProcessor, item);
+        range.insertNode(newTemplateInstance.documentFragment);
+      };
+
+      if (items.length != 0 && templateStruct.placeholder) {
+        templateStruct.placeholder.remove();
+        templateStruct.placeholder = null;
+      }
+    })
+  }
+
+  static _defaultPartProcessor(parts, params) {
     for (const part of parts) {
-        if (part.attribute !== undefined) {
-            part.replaceWith(params[part.expression]);
-        } else if (part.parent !== undefined) {
-            part.replaceWith(params[part.expression]);
-        } else {
-            window.alert("Unknown part type");
-        }
+      part.replaceWith(params[part.expression]);
     }
+  }
+
+  _adjustAttributes(changingPart, adjustment) {
+    for (const part of this._partArray) {
+      if (part != changingPart &&
+        part.node == changingPart.node &&
+        part.attributeName == changingPart.attributeName &&
+        part.start >= changingPart.start) {
+          // this is a part to the right of the changing one, fix it up
+          part.start += adjustment;
+          part.end += adjustment;
+      }
+    }
+  }
 }
 
-TemplateInstance.prototype._adjustAttributes = function (changingPart, adjustment) {
-    for (const part of this.partArray) {
-        if (part != changingPart &&
-            part.node == changingPart.node &&
-            part.attributeName == changingPart.attributeName &&
-            part.start >= changingPart.start) {
-                // this is a part to the right of the changing one, fix it up
-                part.start += adjustment;
-                part.end += adjustment;
-        }
-    }
-}
+//
+// TemplatePart base class
+//
+class TemplatePart {
+  constructor(templateInstance, expression) {
+    this._templateInstance = templateInstance;
+    this._expression = expression;
+  }
 
-TemplateInstance.prototype._adjustText = function (changingPart, adjustment) {
-    for (const part of this.partArray) {
-        if (part != changingPart &&
-            part.node == changingPart.node &&
-            part.start >= changingPart.start) {
-                // this is a part to the right of the changing one, fix it up
-                part.start += adjustment;
-                part.end += adjustment;
-        }
-    }
-}
+  get expression() {
+    return this._expression;
+  }
 
-TemplateInstance.prototype._adjustTextInNewNode = function (changingPart,
-    newNode, adjustment) {
-    for (const part of this.partArray) {
-        if (part != changingPart &&
-            part.node == changingPart.node &&
-            part.start >= changingPart.start) {
-                // this is a part to the right of the changing one, fix it up
-                part.node = newNode;
-                part.start += adjustment;
-                part.end += adjustment;
-        }
-    }
 }
 
 //
 // AttributeTemplatePart class
 //
-function AttributeTemplatePart(templateInstance, node, attributeName, expression, start, end) {
-    this.templateInstance = templateInstance;
-    this.node = node;
-    this.attributeName = attributeName;
-    this.expression = expression;
-    this.start = start;
-    this.end = end;
-}
+class AttributeTemplatePart extends TemplatePart {
+  constructor(templateInstance, node, attributeName, expression, start, end) {
+    super(templateInstance, expression);
+    this._attributeName = attributeName;
+    this._node = node;
+    this._start = start;
+    this._end = end;
+  }
 
-AttributeTemplatePart.prototype.expression = function () {
-    return this.expression;
-}
+  get attribute() {
+    return this._attributeName;
+  }
 
-AttributeTemplatePart.prototype.value = function () {
-    let attributeValue = this.node.getAttribute(this.attributeName);
-    return attributeValue.substring(this.start, this.end);
-}
+  get value() {
+    let rawAttributeValue = this._node.getAttribute(this._attributeName);
+    return rawAttributeValue.substring(this._start, this._end);
+  }
 
-AttributeTemplatePart.prototype.replaceWith = function (newValue) {
-    let attributeValue = this.node.getAttribute(this.attributeName);
-    let oldValue = this.value();
-    if (newValue == undefined) {
-        newValue = "";
+  replaceWith(newPartValue) {
+    let rawAttributeValue = this._node.getAttribute(this._attributeName);
+    let oldPartValue = this.value;
+    if (newPartValue == undefined) {
+      newPartValue = "";
     }
-    attributeValue = attributeValue.substring(0, this.start) +
-        newValue +
-        attributeValue.substring(this.end);
-    this.node.setAttribute(this.attributeName, attributeValue);
-    let adjustment = newValue.length - oldValue.length
-    this.end += adjustment;
-    this.templateInstance._adjustAttributes(this, adjustment);
-}
-
-AttributeTemplatePart.prototype.attribute = function () {
-    return this.attributeName;
+    rawAttributeValue = rawAttributeValue.substring(0, this._start) +
+      newPartValue +
+      rawAttributeValue.substring(this._end);
+    this._node.setAttribute(this._attributeName, rawAttributeValue);
+    let adjustment = newPartValue.length - oldPartValue.length
+    this._end += adjustment;
+    this._templateInstance._adjustAttributes(this, adjustment);
+  }
 }
 
 //
 // NodeTemplatePart class
 //
-function NodeTemplatePart(templateInstance, node, expression, start, end) {
-    this.templateInstance = templateInstance;
+class NodeTemplatePart extends TemplatePart {
+  constructor(templateInstance, node, expression, start, end) {
+    super(templateInstance, expression);
+    this._range = new Range();
+    this._range.setStart(node, start);
+    this._range.setEnd(node, end);
+  }
 
-    this.node = node;
-    this.expression = expression;
-    this.start = start;
-    this.end = end;
-}
+  // // get parent() {
+  // //   return this._range.startContainer.parent;
+  // // }
 
-NodeTemplatePart.prototype.expression = function () {
-    return this.expression;
-}
-
-NodeTemplatePart.prototype.parent = function () {
-    return this.node.parent;
-}
-
-NodeTemplatePart.prototype.replaceWith = function (newValue) {
-    // insert the new value
-    if (newValue !== undefined) {
-        if (newValue.length !== undefined) {
-            // we were given a string
-            this.node.textContent = this.node.textContent.substring(0, this.start) +
-                newValue +
-                this.node.textContent.substring(this.end);
-
-            let oldLength = this.end - this.start;
-            let adjustment = newValue.length - oldLength;
-            this.templateInstance._adjustText(this, adjustment);
-
-        } else {
-            // we were given a node or a document fragment
-            let rangeToRight = document.createRange();
-            rangeToRight.setStart(this.node, this.end);
-            rangeToRight.setEnd(this.node, this.node.textContent.length);
-
-            // remove old value (range will collapse)
-            let range = document.createRange();
-            range.setStart(this.node, this.start);
-            range.setEnd(this.node, this.end);
-            range.deleteContents();
-
-            // insert the new nodes
-            range.insertNode(newValue);
-            this.node.parentElement.normalize();
-
-            if (!rangeToRight.collapsed) {
-                this.templateInstance._adjustTextInNewNode(
-                    this,
-                    rangeToRight.start.node,
-                    0 - this.end);
-            }
-        }
+  replaceWith(newPartValue) {
+    if (newPartValue.length === 0) {
+      newPartValue = ZERO_WIDTH_SPACE;
     }
+
+    if (newPartValue !== undefined) {
+      let newNode = (newPartValue.length !== undefined) ?
+                    new Text(newPartValue) :
+                    newPartValue;
+      this._range.deleteContents();
+      let parent = this._range.startContainer;
+      if (parent.nodeType === Node.TEXT_NODE) {
+        parent = parent.parentElement;
+      }
+      this._range.insertNode(newNode);
+      if (parent == null) {
+        window.alert(this._range.startContainer);
+      }
+      parent.normalize();
+    }
+  }
 }
+
 
 //
 // PartParser class
 //
-function PartParser(templateInstance) {
-    this.templateInstance = templateInstance;
-    this.partArray = [];
-    this.imbeddedTemplateArray = [];
+class PartParser {
+  constructor(templateInstance) {
+    this._templateInstance = templateInstance;
+    this._partArray = [];
+    this._internalTemplateNodeArray = [];
+
     this._findPartsInFragment(templateInstance.documentFragment);
-}
+  }
 
-PartParser.prototype.partArray = function() {
-    return this.partArray;
-}
+  get parts() {
+    return this._partArray;
+  }
 
-PartParser.prototype.imbeddedTemplateArray = function() {
-    return this.imbeddedTemplateArray;
-}
+  get internalTemplateNodes() {
+    return this._internalTemplateNodeArray;
+  }
 
-PartParser.prototype._findPartsInFragment = function (documentFragment) {
+  _findPartsInFragment(documentFragment) {
     for (let child = documentFragment.firstChild;
-        child != null;
-        child = child.nextSibling) {
-        this._findPartsInTree(child);
+         child != null;
+         child = child.nextSibling) {
+      this._findPartsInTree(child);
     }
-}
+  }
 
-PartParser.prototype._findPartsInTree = function (root) {
+  _findPartsInTree(root) {
     let walker = document.createTreeWalker(
-        root,
-        NodeFilter.SHOW_ELEMENT | NodeFilter.SHOW_TEXT);
-    do {
-        let node = walker.currentNode;
-        if (node.nodeType == Node.ELEMENT_NODE) {
-            this._findPartsInElementNode(node);
-        } else if (node.nodeType == Node.TEXT_NODE) {
-            this._findPartsInTextNode(node);
+      root,
+      NodeFilter.SHOW_ELEMENT | NodeFilter.SHOW_TEXT);
+    let node = walker.currentNode;
+    while (node) {
+      if (node.nodeType == Node.ELEMENT_NODE) {
+        if (node.tagName == "TEMPLATE") {
+          this._internalTemplateNodeArray.push(node);
+          node = walker.nextSibling();
+        } else {
+          this._findPartsInElementNode(node);
+          node = walker.nextNode();
         }
-    } while (walker.nextNode());
-}
+      } else if (node.nodeType == Node.TEXT_NODE) {
+        this._findPartsInTextNode(node);
+        node = walker.nextNode();
+      } else {
+        window.alert("_findPartsInTree ran across a " + node.nodeType);
+      }
+    };
+  }
 
-PartParser.prototype._findPartsInElementNode = function (elementNode) {
+  _findPartsInElementNode(elementNode) {
     let attributeNames = elementNode.getAttributeNames();
     for (let i = 0; i < attributeNames.length; i++) {
-        let attributeName = attributeNames[i];
-        let attributeValue = elementNode.getAttribute(attributeName);
-        let part = this._findNextPart(attributeValue);
-        while (part != null) {
-            this.partArray.push(
-                new AttributeTemplatePart(this.templateInstance, elementNode,
-                    attributeName, part.id, part.start, part.end));
-
-            part = this._findNextPart(attributeValue, part.end);
-        }
+      let attributeName = attributeNames[i];
+      let attributeValue = elementNode.getAttribute(attributeName);
+      let part = PartParser._findNextPart(attributeValue);
+      while (part != null) {
+        this._partArray.push(
+          new AttributeTemplatePart(this._templateInstance, elementNode,
+              attributeName, part.id, part.start, part.end));
+        part = PartParser._findNextPart(attributeValue, part.end);
+      }
     }
-    if (elementNode.tagName == "TEMPLATE") {
-        this.imbeddedTemplateArray.push(elementNode);
-    }
-}
+  }
 
-PartParser.prototype._findPartsInTextNode = function (textNode) {
+  _findPartsInTextNode(textNode) {
     let text = textNode.textContent;
-    let part = this._findNextPart(text);
+    let part = PartParser._findNextPart(text);
     while (part != null) {
-        this.partArray.push(
-            new NodeTemplatePart(this.templateInstance, textNode,
-                part.id, part.start, part.end));
-
-        part = this._findNextPart(text, part.end);
+      this._partArray.push(
+        new NodeTemplatePart(this._templateInstance, textNode,
+            part.id, part.start, part.end));
+      part = PartParser._findNextPart(text, part.end);
     }
-}
+  }
 
-// returns the gap before the starting {{ and the gap after the ending }}
-// {start:#, end:#, id:string}
-PartParser.prototype._findNextPart = function (string, base = 0) {
-    let beforeOpenMustacheOffsetFromBase = string.substring(base).search("{{");
-    if (beforeOpenMustacheOffsetFromBase != -1) {
-        let beforeCloseMustacheOffsetFromBase = string.substring(base).search("}}");
-        if (beforeCloseMustacheOffsetFromBase != -1) {
-            return {
-                start: beforeOpenMustacheOffsetFromBase + base,
-                end: beforeCloseMustacheOffsetFromBase + base + 2,
-                id: string.substring(
-                    beforeOpenMustacheOffsetFromBase + base + 2,
-                    beforeCloseMustacheOffsetFromBase + base
-                )
-            };
-        }
+  // returns the gap before the starting {{ and the gap after the ending }}
+  // {start:#, end:#, id:string}
+  static _findNextPart(string, base = 0) {
+    let beforeOpenTagOffsetFromBase = string.substring(base).search("{{");
+    if (beforeOpenTagOffsetFromBase != -1) {
+      let beforeCloseTagOffsetFromBase = string.substring(base).search("}}");
+      if (beforeCloseTagOffsetFromBase != -1) {
+        return {
+          start: beforeOpenTagOffsetFromBase + base,
+          end: beforeCloseTagOffsetFromBase + base + 2,
+          id: string.substring(
+            beforeOpenTagOffsetFromBase + base + 2,
+            beforeCloseTagOffsetFromBase + base
+          )
+        };
+      }
     }
     return null;
+  }
 }
-
